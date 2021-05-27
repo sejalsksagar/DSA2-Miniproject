ba1a9ee7cdd9df17252502a5f4722eaeaac2a5c5
ketakikothale <56468710+ketakikothale@users.noreply.github.com>
2021-05-27 18:17:52 +0530

    Add files via upload

--- miniProject/Twitter.java
+++ miniProject/Twitter.java
@@ -2,183 +2,202 @@ package miniProject;
 
 import java.util.*;
 
-public class Twitter {
-	//graph using Adjacency list
+public class Twitter
+{
+	// graph using Adjacency list
 	static ArrayList<AccGNode> GHead;
-	static AccTNode accRoot;//avl
-	
-	Twitter(){
+	static AccTNode accRoot;// avl
+
+	Twitter()
+	{
 		GHead = new ArrayList<AccGNode>();
 		accRoot = null;
 	}
-	
-	int height(AccTNode root){
-        int lh, rh;
-        if(root == null)
-            return 0;
 
-        if(root.left == null)
-            lh = 0;
-        else
-            lh = 1 + root.left.h;
+	int height(AccTNode root)
+	{
+		int lh, rh;
+		if (root == null)
+			return 0;
 
-        if(root.right == null)
-            rh = 0;
-        else
-            rh = 1 + root.right.h;
+		if (root.left == null)
+			lh = 0;
+		else
+			lh = 1 + root.left.h;
 
-        if(lh > rh)
-            return lh;
-        else
-            return rh;
-    }
+		if (root.right == null)
+			rh = 0;
+		else
+			rh = 1 + root.right.h;
 
-    int balanceFactor(AccTNode root){
-        int bf, lh, rh;
+		if (lh > rh)
+			return lh;
+		else
+			return rh;
+	}
 
-        if(root == null)
-            return 0;
-        
-        if(root.left == null)
-            lh = 0;
-        else 
-            lh = 1 + height(root.left);
+	int balanceFactor(AccTNode root)
+	{
+		int bf, lh, rh;
 
-        if(root.right == null)
-            rh = 0;
-        else 
-            rh = 1 + height(root.right);
+		if (root == null)
+			return 0;
 
-        bf = lh - rh;
-        return bf;
-    }
+		if (root.left == null)
+			lh = 0;
+		else
+			lh = 1 + height(root.left);
 
+		if (root.right == null)
+			rh = 0;
+		else
+			rh = 1 + height(root.right);
 
-    AccTNode LL(AccTNode root){ 
-        //Right Rotation
-    	AccTNode tmp = root.left;
-        root.left = tmp.right;
-        tmp.right = root;
-        tmp.h = height(tmp);
-        root.h = height(root);
-        return tmp;
-    }
+		bf = lh - rh;
+		return bf;
+	}
 
-    AccTNode RR(AccTNode root){ 
-        //Left Rotation
-    	AccTNode tmp = root.right;
-        root.right = tmp.left;
-        tmp.left = root;
-        tmp.h = height(tmp);
-        root.h = height(root);
-        return tmp;
-    }
+	AccTNode LL(AccTNode root)
+	{
+		// Right Rotation
+		AccTNode tmp = root.left;
+		root.left = tmp.right;
+		tmp.right = root;
+		tmp.h = height(tmp);
+		root.h = height(root);
+		return tmp;
+	}
 
-    AccTNode LR(AccTNode root){ 
-        //1) Left Rotation around child(alpha)
-        //2) Right Rotation around alpha
-        root.left = RR(root.left);
-        root = LL(root);
-        return root;
-    }
+	AccTNode RR(AccTNode root)
+	{
+		// Left Rotation
+		AccTNode tmp = root.right;
+		root.right = tmp.left;
+		tmp.left = root;
+		tmp.h = height(tmp);
+		root.h = height(root);
+		return tmp;
+	}
 
-    AccTNode RL(AccTNode root){ 
-        //1) Right Rotation around child(alpha)
-        //2) Left Rotation around alpha
-        root.right = LL(root.right);
-        root = RR(root);
-        return root;
-    }
-	
-    AccTNode insert(AccTNode root, Account A){ 
-        int bf;
-        if(root == null){
-            root = new AccTNode(A);
-            return root;
-        }
+	AccTNode LR(AccTNode root)
+	{
+		// 1) Left Rotation around child(alpha)
+		// 2) Right Rotation around alpha
+		root.left = RR(root.left);
+		root = LL(root);
+		return root;
+	}
 
-        if(A.username.compareToIgnoreCase(root.A.username) < 0){
-            root.left = insert(root.left, A);
-            bf = balanceFactor(root);
+	AccTNode RL(AccTNode root)
+	{
+		// 1) Right Rotation around child(alpha)
+		// 2) Left Rotation around alpha
+		root.right = LL(root.right);
+		root = RR(root);
+		return root;
+	}
 
-            if(bf == 2){
-                if(A.username.compareToIgnoreCase(root.left.A.username) < 0)
-                    root = LL(root);
-                else
-                    root = LR(root);
-            }
-        }
-        else{ 
-            root.right = insert(root.right, A);
-            bf = balanceFactor(root);
+	AccTNode insert(AccTNode root, Account A)
+	{
+		int bf;
+		if (root == null)
+		{
+			root = new AccTNode(A);
+			return root;
+		}
 
-            if(bf == -2){
-                if(A.username.compareToIgnoreCase(root.right.A.username) > 0)
-                    root = RR(root);
-                else
-                    root = RL(root);
-            }
-        }
-        root.h = height(root);
-        return root;
-    }
-    
-    static Account searchUsername(String un){ 
-        AccTNode ptr = accRoot;
-        boolean flag = false;
+		if (A.username.compareToIgnoreCase(root.A.username) < 0)
+		{
+			root.left = insert(root.left, A);
+			bf = balanceFactor(root);
 
-        while(ptr != null){
-            if(un.compareToIgnoreCase(ptr.A.username) == 0){
-                flag = true;
-                break;
-            }
-            if(un.compareToIgnoreCase(ptr.A.username) < 0)
-                ptr = ptr.left;
-            else 
-                ptr = ptr.right;
-        }
+			if (bf == 2)
+			{
+				if (A.username.compareToIgnoreCase(root.left.A.username) < 0)
+					root = LL(root);
+				else
+					root = LR(root);
+			}
+		}
+		else
+		{
+			root.right = insert(root.right, A);
+			bf = balanceFactor(root);
 
-        if(flag)
-            return ptr.A;
-        else
-            return null;
-    }
-    
-    static Account searchUsernameGraph(String un){ 
-        Iterator<AccGNode> ptr = GHead.iterator();
+			if (bf == -2)
+			{
+				if (A.username.compareToIgnoreCase(root.right.A.username) > 0)
+					root = RR(root);
+				else
+					root = RL(root);
+			}
+		}
+		root.h = height(root);
+		return root;
+	}
 
-        while(ptr.hasNext())
-        {
-        	Account A1=ptr.next().A;
-        	if(un.compareToIgnoreCase(A1.username)==0)
-                return A1;
-        }
-        
-       return null;        
-    }
-    
-	void createAccount(Scanner sc) {
+	static Account searchUsername(String un)
+	{
+		AccTNode ptr = accRoot;
+		boolean flag = false;
+
+		while (ptr != null)
+		{
+			if (un.compareToIgnoreCase(ptr.A.username) == 0)
+			{
+				flag = true;
+				break;
+			}
+			if (un.compareToIgnoreCase(ptr.A.username) < 0)
+				ptr = ptr.left;
+			else
+				ptr = ptr.right;
+		}
+
+		if (flag)
+			return ptr.A;
+		else
+			return null;
+	}
+
+	static Account searchUsernameGraph(String un)
+	{
+		Iterator<AccGNode> ptr = GHead.iterator();
+
+		while (ptr.hasNext())
+		{
+			Account A1 = ptr.next().A;
+			if (un.compareToIgnoreCase(A1.username) == 0)
+				return A1;
+		}
+
+		return null;
+	}
+
+	void createAccount(Scanner sc)
+	{
 		Account A = new Account();
 		String un;
-		do {
+		do
+		{
 			System.out.print("Enter username: ");
 			un = sc.nextLine();
-			if(searchUsername(un) != null)
+			if (searchUsername(un) != null)
 				System.out.println("Entered username already exits.");
-		}while(searchUsername(un) != null);
+		} while (searchUsername(un) != null);
 		A.username = un;
 		A.accept(sc);
-		
-		//add user to graph
+
+		// add user to graph
 		AccGNode a = new AccGNode(A);
 		GHead.add(a);
-		
-		//add account to avl tree
+
+		// add account to avl tree
 		accRoot = insert(accRoot, A);
-		
+
 		System.out.println("Your account has been successfully created!");
 	}
-	
+
 	void login(Scanner sc)
 	{
 		String un;
@@ -225,63 +244,139 @@ public class Twitter {
 			A.activity(sc, A, GHead);
 	}
 	
-	static void explore(Scanner sc,Account A,ArrayList<AccGNode> GHead) {
+	//ignore this
+	//this is to debug
+	static void display()
+	{
+		for(AccGNode ii:GHead)
+		{
+			AccGNode pp=ii;
+			while(pp!=null)
+			{
+				System.out.print(pp.A.username+"->");
+				pp=pp.link;
+			}
+			System.out.println();
+		}
+	}
+
+	static void explore(Scanner sc, Account A, ArrayList<AccGNode> GHead)
+	{
 		byte ch;
-		do {
+		do
+		{
 			System.out.println("____________________________________");
 			System.out.println("********* EXPLORE **********");
 			System.out.println("0. Back");
 			System.out.println("1. Search Username ");
 			System.out.println("2. Follow a user");
-			//most popular user
+			System.out.println("3. Unfollow a user");
+			System.out.println("8.Debug Graph");
+			// most popular user
 			System.out.print("Enter your choice: ");
 			ch = sc.nextByte();
 			sc.nextLine();
 			System.out.println("____________________________________");
-			switch(ch) {
-			case 0: break;
-			
-			case 1: System.out.print("Enter username to search: ");
+			switch (ch)
+			{
+				case 0:
+					break;
+
+				case 1:
+					System.out.print("Enter username to search: ");
 					Account S = searchUsername(sc.nextLine());
-					if(S == null)
+					if (S == null)
 						System.out.println("Username not found");
-					else {
+					else
+					{
 						System.out.println("Username found");
 						S.viewProfile();
-						//follow? / unfollow? / following?
+						// follow? / unfollow? / following?
+					}
+					break;
+
+				case 2:
+					System.out.print("Enter username to follow: ");
+					String search = sc.nextLine();
+					Account F = searchUsernameGraph(search);
+					if (F == null)
+						System.out.println("Username not found");
+					else
+					{
+						boolean isAlreadyFollow = false;
+						for (AccGNode gNode : GHead)
+						{
+							if (gNode.A.username == A.username)
+							{
+								AccGNode ptr = gNode;
+								while (ptr != null)
+								{
+									if (ptr.A.username == F.username)
+									{
+										System.out.println("You already follow this account");
+										isAlreadyFollow = true;
+										break;
+									}
+									ptr = ptr.link;
+								}
+							}
+						}
+						for (AccGNode gN : GHead)
+						{
+							if (gN.A.username == A.username && !isAlreadyFollow)
+							{
+								AccGNode temp = new AccGNode(F);
+								if (gN.link == null)
+									gN.link = temp;
+								else
+								{
+									temp.link = gN.link;
+									gN.link = temp;
+								}
+								A.friendsCount++;
+								F.followersCount++;
+								System.out.println("Username found");
+								System.out.println("You now follow @" + search);
+								break;
+							}
+						}
 					}
 					break;
 					
-			case 2:
-				System.out.print("Enter username to follow: ");
-				String search = sc.nextLine();
-				Account F = searchUsernameGraph(search);
-				if(F == null)
-					System.out.println("Username not found");
-				else
-				{
-					for(AccGNode gN : GHead)
+				case 3:
+					System.out.print("Enter username to unfollow: ");
+					String search1 = sc.nextLine();
+					for (AccGNode gN : GHead)
 					{
-						if(gN.A == F)
+						if (gN.A.username == A.username)
 						{
-							AccGNode temp = new AccGNode(F);
-							if(gN.link == null)
-								gN.link = temp;
+							AccGNode locate = gN.link;
+							AccGNode prev = gN.link;
+							for (locate = gN.link, prev = gN.link; locate != null && locate.A.username != search1;
+									prev = locate, locate = locate.link)
+							{
+								System.out.println(locate.A.username);
+							}
+							if (locate == null)	
+								System.out.println("You dont follow "+ search1);
+							else if (locate.A.username == gN.A.username)//first node
+							{
+								gN.link = locate.link;
+								System.out.println("You unfollowed "+ search1);
+							}
 							else
 							{
-								temp.link = gN.link;
-								gN.link = temp;
+								prev.link = locate.link;
+								System.out.println("You unfollowed "+ search1);
 							}
-							A.friendsCount++;
-							F.followersCount++;
 							break;
 						}
 					}
-					System.out.println("Username found");
-					System.out.println("You now follow @" + search);
-				}
-				break;
+					break;
+					
+				case 8:
+					display();
 			}
-		}while(ch != 0);
+		} while (ch != 0);
 	}
 }
